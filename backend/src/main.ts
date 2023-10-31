dc1f7596adbdbd372fe010947eff4b56e4073322
Hanjunhee-1 <hanjuni857@naver.com>
2023-11-01 02:41:35 +0900

    feat: swagger, cors, pipe 관련 설정

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,44 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Global Pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  // CORS
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  });
+
+  // swagger
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('E-code helper')
+    .setDescription('소프트웨어공학개론 TEAM 5 작품')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        in: 'header',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(8000);
 }
 bootstrap();
