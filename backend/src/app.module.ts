75d3dc46581f3cb3ec9a97cd6550de65e3a638bf
Hanjunhee-1 <hanjuni857@naver.com>
2023-11-01 04:08:19 +0900

    feat: user 모듈 생성

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, UsersModule],
 })
 export class AppModule {}
