2d19480ffdb77d94c2a36917db48ca9410bcecea
Hanjunhee-1 <hanjuni857@naver.com>
2023-11-01 02:48:31 +0900

    feat: PrismaModule, AuthModule 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, AuthModule],
 })
 export class AppModule {}
