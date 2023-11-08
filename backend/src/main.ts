e63e532e8e457b9bf03fbe28c608b59992911c7b
bongyeob2 <bongyeob2@gmail.com>
2023-11-08 20:25:05 +0900

    fix: jwt error dotenv로 해결

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
