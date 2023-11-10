065793ea63111a81f827c34b5d46c0bddcc98d4a
changu81 <ydi990801@gmail.com>
2023-11-10 16:19:11 +0900

    Change directory

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Header from './Header';
-import Content from './Content';
+import Header from './Content/Header';
+import Content from './Content/Content';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
@@ -11,8 +11,11 @@ function App() {
       <div className="App">
       <Routes>
         <Route path='/'  element={<Content/>}></Route>
+        {/* 로그인 페이지 구현하고 element 교체 */}
         <Route path='/login'  element={<Content/>}></Route>
+        {/* 회원가입 페이지 구현하고 element 교체 */}
         <Route path='/signup'  element={<Content/>}></Route>
+        {/* 기록 페이지 구현하고 element 교체 */}
         <Route path='/record'  element={<Content/>}></Route>
         </Routes>
       </div>
