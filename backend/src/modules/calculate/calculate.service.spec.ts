03de055118181b7fa5c9d09c607e410bfbd6f8a9
wjdqudalsqordjq <jack2025@naver.com>
2023-11-13 03:59:27 +0900

    test: 테스트케이스 추가

--- backend/src/modules/calculate/calculate.service.spec.ts
+++ backend/src/modules/calculate/calculate.service.spec.ts
@@ -1,12 +1,12 @@
 // calculate.service.spec.ts
 import { Test, TestingModule } from '@nestjs/testing';
 import { CalculateService } from './calculate.service';
-import * as fs from 'fs';
-import * as childProcess from 'child_process';
+
+jest.setTimeout(10000);
 
 describe('CalculateService', () => {
   let service: CalculateService;
-
+  
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [CalculateService],
@@ -14,14 +14,45 @@ describe('CalculateService', () => {
 
     service = module.get<CalculateService>(CalculateService);
   });
-
-  it('should execute Java code and return execution time', async () => {
-    const javaCode = 'class aaaa { public static void main(String[] args) {System.out.println("Hello, World!");}}';
-
-    const capturedOutput = await service.executeCodeAndGetTime(javaCode);
   
-    // 여기에서 모의 Java 코드 실행 결과를 확인
+  it('should execute Java code and return execution time', async () => {
+    const testJavaCode1 = 'public class aaaa { public static void main(String[] args) {System.out.println("Hello, World!");}}';
+    const testJavaCode2 = 'class bbbb { public static void main(String[] args) {System.out.println("Hello, World!");}}';
+    const testJavaCode3 = 'class cccc { public static void main(String[] args) {System.out.println("Hello, World!");}} public class aaaa { public static void main(String[] args) {System.out.println("Hello, World!");}}';    
+
+    const capturedOutput1 = await service.executeCodeAndGetTime(testJavaCode1);
+    const capturedOutput2 = await service.executeCodeAndGetTime(testJavaCode2);
+    const capturedOutput3 = await service.executeCodeAndGetTime(testJavaCode3);
+
+    expect(capturedOutput1).toBeDefined();
+    expect(capturedOutput1).toBeGreaterThanOrEqual(0);
+    expect(capturedOutput2).toBeDefined();
+    expect(capturedOutput2).toBeGreaterThanOrEqual(0);
+    expect(capturedOutput3).toBeDefined();
+    expect(capturedOutput3).toBeGreaterThanOrEqual(0);
+  });
+
+  it('compile failure and should return -1', async() => {
+    const testJavaCode = 'public class cccc { public static void main(String[] args) {System.out.println("Hello, World!")}}';
+    const capturedOutput = await service.executeCodeAndGetTime(testJavaCode);
+
     expect(capturedOutput).toBeDefined();
-    expect(capturedOutput).toBeGreaterThanOrEqual(0);
+    expect(capturedOutput).toEqual(-1);
+  });
+
+  it('too long execution and should return -2', async() => {
+    const testJavaCode = 'public class cccc { public static void main(String[] args) {while(true);}}';
+    const capturedOutput = await service.executeCodeAndGetTime(testJavaCode);
+
+    expect(capturedOutput).toBeDefined();
+    expect(capturedOutput).toEqual(-2);
+  });
+
+  it('run time error and should return -3', async() => {
+    const testJavaCode = 'class bbbb { public static void main(String[] args) {String str = null; System.out.println(str.length());}}';
+    const capturedOutput = await service.executeCodeAndGetTime(testJavaCode);
+
+    expect(capturedOutput).toBeDefined();
+    expect(capturedOutput).toEqual(-3);
   });
 });
