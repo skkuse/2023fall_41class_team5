30eae065a4f39a6661e23d07812cd821d4290e98
bongyeob2 <bongyeob2@gmail.com>
2023-11-14 01:18:26 +0900

    feat: run time error handling 추가

--- backend/src/modules/calculate/calculate.service.ts
+++ backend/src/modules/calculate/calculate.service.ts
@@ -14,11 +14,11 @@ export class CalculateService {
 
     fs.writeFileSync(`./code/${fileName}`, javaCode);
 
-    var isSuccess = false;
+    var isCPSuccess = false;
     try{
       const result = await this.executeJavaCode(fileName);
       const executionTime = this.converTimeFormatToSeconds(result);
-      isSuccess = true;
+      isCPSuccess = true;
       ///////
       //db 저장 구현
 
@@ -32,37 +32,49 @@ export class CalculateService {
         // ./code 안에 생성된 파일 모두 삭제
         this.deleteFilesInDirectory('./code');
         
-        if (!isSuccess)
+        if (!isCPSuccess)
           return -1;
     }
   }
 
   private async executeJavaCode(fileName: string): Promise<string> {
-
     return new Promise<string>((resolve, reject) => {
       const process = childProcess.spawn('javac', [`./code/${fileName}`]);
-
+  
       process.on('close', (code) => {
         if (code === 0) {
           const javaExecutionCommand = `sh -c 'time java ./code/${fileName}'`;
           const javaExecution = childProcess.spawn(javaExecutionCommand, { shell: true });
-          
+  
           let result = '';
-          
-          //자바 코드 실행 시간 (time)
+  
+          // 자바 코드 실행 결과 수집
+          javaExecution.stdout.on('data', (data) => {
+            result += data.toString();
+          });
+  
+          // 자바 코드 실행 시간 (time)
           javaExecution.stderr.on('data', (error) => {
             result += error.toString();
-          })
-
-          javaExecution.on('close', () => {
-            resolve(result);
+          });
+  
+          javaExecution.on('close', (exitCode) => {
+            console.log(`log  ${fileName} execution completed with exit code ${exitCode}`);
+            console.log(`log  ${fileName} execution result: ${result}`);
+            
+            if (exitCode === 0) { // 정상적으로 실행
+              resolve(result);
+            } else {  // run time error 발생 시 -3을 converTimeFormatToSeconds로 전달
+              resolve('-3'); 
+            }
           });
         } else {
           reject(`Java compilation failed with code ${code}`);
         }
       });
     });
-  }
+  }  
+
 
   private extractClassName(javaCode: string): string | null {
     const classNameMatch = javaCode.match(/public\s+class\s+(\w+)/);
@@ -71,7 +83,7 @@ export class CalculateService {
 
   private converTimeFormatToSeconds(timeString: string): number | null{
     const match = timeString.match(/(\d+\.\d+)s/);
-    return match ? parseFloat(match[1]) : null;
+    return match ? parseFloat(match[1]) : -3; // run time error 발생 시 -3 return
   }
 
   private deleteFilesInDirectory(directoryPath: string): void {
