09200798ef5da42cae070a1520a1230c87b919bb
Hanjunhee-1 <hanjuni857@naver.com>
2023-11-16 13:11:05 +0900

    feat: PostModule 생성 및 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,8 +3,15 @@ import { AuthModule } from './modules/auth/auth.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { UsersModule } from './modules/users/users.module';
 import { InformationModule } from './modules/information/information.module';
+import { PostModule } from './modules/post/post.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule, InformationModule],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    InformationModule,
+    PostModule,
+  ],
 })
 export class AppModule {}
