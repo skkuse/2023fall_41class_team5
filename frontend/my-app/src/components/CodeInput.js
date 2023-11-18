1c5ed9f223afec7697db2c235cd3563db0e38230
changu81 <ydi990801@gmail.com>
2023-11-18 21:16:32 +0900

    Try to submit java code

--- frontend/my-app/src/components/CodeInput.js
+++ frontend/my-app/src/components/CodeInput.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Editor from '@monaco-editor/react';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const CodeContainer = styled.div`
   display: flex;
@@ -22,12 +23,39 @@ const editorOptions = {
   minimap:{enabled: false}
 }
 
+
+
 function CodeInput(){
+  const [javaCode, setJavaCode] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch('/calculate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ javaCode }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      console.log('Server Response:', data);
+
+      // 서버 응답에 대한 추가 로직을 여기에 추가할 수 있습니다.
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
     return <div className='codeInput' >
       <article>
-        <form onSubmit={event=>{
-          //제출 시 이벤트 구현?
-        }}> 
+        <form onSubmit={handleSubmit}>
           <CodeContainer>
             <p>아래에 JAVA 코드를 입력하세요</p>
             <Editor
